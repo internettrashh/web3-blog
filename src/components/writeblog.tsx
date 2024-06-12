3dcd95cf42fcd4ba4e42a0729736af7c568b2457
internet_trash <naiknischal92@gmail.com>
2024-06-13 02:11:26 +0530

    add wallet connect conditions

--- src/components/writeblog.tsx
+++ src/components/writeblog.tsx
@@ -5,23 +5,36 @@ import { CardHeader, CardContent, Card } from "@/components/ui/card";
 import { useNavigate } from "react-router-dom";
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css'; // Import the theme you want to use
+import { ConnectButton, useConnection } from "@arweave-wallet-kit/react";
 
 export function Writeblog() {
   const navigate = useNavigate();
   const [text, setText] = React.useState('');
-
+  const { connected ,connect } = useConnection();
+  
+  
   return (
-    <div className="max-w-6xl mx-auto mt-8 pt-16">
-      <h1 className="text-4xl font-bold mb-4">Welcome to the AO Blog</h1>
-      <p className="text-gray-600 mb-8">Discover the latest news, insights, and updates from the Vercel team.</p>
-      <div className="flex flex-col items-center">
-        <div style={{ height: '500px', width: '100%' }}> {/* Adjusted width to 100% */}
-          <ReactQuill theme="snow" value={text} onChange={setText} style={{ height: '100%', width: '100%' }} />
+    connected ? 
+    (
+      <div className="max-w-6xl mx-auto mt-8 pt-16">
+        <h1 className="text-4xl font-bold mb-4">Share your thoughts</h1>
+        <p className="text-gray-600 mb-8">Discover the latest news, insights, and updates from the Vercel team.</p>
+        <div className="flex flex-col items-center">
+          <div style={{ height: '500px', width: '100%' }}>
+            <ReactQuill theme="snow" value={text} onChange={setText} style={{ height: '100%', width: '100%' }} />
+          </div>
+          <Button className='self-end mt-16 w-full'>
+            Post
+          </Button>
         </div>
-        <Button className='self-end mt-16 w-full'>
-          Post
-        </Button>
       </div>
-    </div>
+    ) : 
+    (
+      <div className="max-w-6xl mx-auto mt-8 pt-16">
+      <div className="text-center mt-8 text-4xl font-bold mb-4">
+        <p>Please connect your wallet to create posts.</p>
+      </div>
+      </div>
+    )
   );
 } 
